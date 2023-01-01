1c19973b20ed89eb1509ce15e50776ebfeb39c51
Raptor7742 <106883599+Raptor7742@users.noreply.github.com>
2023-01-01 12:24:30 +0100

    Update script.js

--- script.js
+++ script.js
@@ -37,12 +37,12 @@ const playerInstance = jwplayer("player").setup({
         },
         {
           file:
-            "https://2448361416.tapecontent.net/radosgw/7X9kKY8gG6hAgbg/gwU0zxxqtpqP-C_Hh5d_AMFRBjevAL67cav5DquCR-wOy8I78fTh8XujOS4LfTKcVGnpd0QN0eBml6dtK8MOzhb0MJF6OcIxixmgDQn7xpmHVXF6sowBx_I2XaIkpBKh_dQ6njPlOkVaaST9oI2HvAoGTrjaGgSoz9i7S6taVL2I6OzptoIKEgzlWXrvLHutEEszQJEQDpF90nnrga_HJ7UuiCz0rmQ4nprIRhY_DKf4iz9matmRw3AEjCsFDMJnIbeDU54z4BRu9ZLfZO3pInGYdqhQozkhbfLtmvJeJLmGHtavSY5TkkKCO9c/One%2BPiece%2BFilm%2BRed%2B-%2BVOSTFR.mp4?stream=1",
+            "",
           label: "720p"
         },
         {
           file:
-            "https://m103.syncusercontent.com/mfs-60:74f830b6798561e212b29389fa3727ec=============================/p/2.mp4?allowdd=0&datakey=QUR/XZHCA0iIjnS/SG3XtO5Dg2zOKgWMD63p+nKILx+r7zjOPDHSKk4jewQLVn0lvJH+EGjx2Yx67vp9Wz9LuzK3isLjz8A3O33Iti70SR/Q2zgrCFM9vPrBWtXh4dZMPc7RbXJT4PWVL8nXGSpUmof2fz+03ZBQtsslttwBXgTt/cmQCwrLXSScdKeejGYqx7oZP15t/03sj3U8vRGNJC7e57JJcB4n2yCazDKz77mvS5XyMsZzK1iNDoOMUtpbseXRpBumTftcE1IvnB8EoKrgdWDMakhYS1n44+BqJ3caplvSkzUgrq237LWuLwZtgddefGA4s7+Co7hVzGp4/g&engine=ln-2.3.7.3&errurl=Ak3APrkajIY0pI4tQIdNPcmEoU53sVibSQ5qC3ljRGiFGWGURkyEEDlwsEiQEUn+8/zRE4PnpjZlmDDaKzKblmw11dilUqCMBUye3r1MF1SyGNUazlUSMHnbYDeRlcyqK+cDvcMtxUwyhSIBgw3nYbl7e8sEdPngIpVwcNp05WkbtNCR3vmkwCVxJamXTpCL/dtQFU/5vkoMv7L9BmNSwil8ROzurMi/0D9JqinQaDPamwTL/tAYTnmm6ce0QHSpA44+bGlC5D87YI2tfoAY+8fiPRtc4rEW/8gikMEIPUd/AN8l9Uo3uEhfG8QZm7izNbm/c7XA31o47TOm1Juisg==&header1=Q29udGVudC1UeXBlOiB2aWRlby9tcDQ&header2=Q29udGVudC1EaXNwb3NpdGlvbjogaW5saW5lOyBmaWxlbmFtZT0iMi5tcDQiO2ZpbGVuYW1lKj1VVEYtOCcnMi5tcDQ7&ipaddress=1458994159&linkcachekey=1039cc5e0&linkoid=1542710012&mode=101&sharelink_id=8007941330012&timestamp=1672514588488&uagent=1d9728cd21b3c3ecf5078376d4b371f2c858e9ed&signature=ff0fc22a19676408d6308ca60d2a045739355a6d&cachekey=60:74f830b6798561e212b29389fa3727ec=============================",
+            "",
           label: "480p"
         },
         {
